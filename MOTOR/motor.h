829a833bfadbec97d55e9563f5df47cb45d09787
eamon <ruanyiming71@gmail.com>
2025-08-07 10:19:56 +0800

    Add files via upload

---
+++ MOTOR/motor.h
@@ -0,0 +1,6 @@
+#include "ti_msp_dl_config.h"
+
+void Motor_Enable(void);//使能电机
+void Set_Speed_Mode_CW(void); //电机以200转顺时针旋转
+void Set_Speed_Mode_CWW(void); //电机以200转逆时针旋转
+void stop_motor(void);//停止旋转
