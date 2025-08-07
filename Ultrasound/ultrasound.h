611487460e17b1f0059ca956ee1068aa26f5cf0d
eamon <ruanyiming71@gmail.com>
2025-08-07 10:25:44 +0800

    Create ultrasound.h

---
+++ Ultrasound/ultrasound.h
@@ -0,0 +1,12 @@
+#include "ti_msp_dl_config.h"
+
+#ifndef GPIO_Ultrasonic_PIN_Trig_PORT
+#define GPIO_Ultrasonic_PIN_Trig_PORT GPIO_Ultrasonic_PORT
+#endif
+
+#ifndef GPIO_Ultrasonic_PIN_Echo_PORT
+#define GPIO_Ultrasonic_PIN_Echo_PORT GPIO_Ultrasonic_PORT
+#endif
+
+uint32_t getUltrasonicDist(void);
+void ultrasonic_GetStatus(void);
